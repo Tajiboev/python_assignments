eb377fec0d0f8951f2777728b41d1d850471b8f9
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2019-10-12 23:47:28 +0900

    still working on the assignment #1, just finished sell_share()

--- assignment01/stock_manager.py
+++ assignment01/stock_manager.py
@@ -59,21 +59,21 @@ class StockManager:
 
     def buy_multiple(self, company_list, number_list, buy_price_list):
         
-        if len(company_list) != len(number_list) != len(buy_price_list):
+        samelength = (len(company_list) == len(number_list) == len(buy_price_list))
+        
+        if not samelength:
             print('Error: Lengths of lists are not equal')
-            return
-
-        orders = len(company_list)
-        for i in range(0, orders):
-            order = [company_list[i], number_list[i], buy_price_list[i]]
+            return False
+ 
+        for i in range(0, len(company_list)):
             try:
-                self.buy_shares(order[0], order[1], order[2])
+                self.buy_shares(company_list[i], number_list[i], buy_price_list[i])
             except ValueError:
                 raise
             except TypeError:
                 raise
 
-    def sell_share(self, company, number, sell_price):
+    def sell_share(self, company, number, sell_price, profit = 0):
         if company not in self.companies:
             raise ValueError(f'"{company}" is not managed in the app.')    
         
@@ -88,44 +88,33 @@ class StockManager:
         elif sell_price <= 0:
             raise ValueError(f'Sell price cannot be negative')
         
-        print('printing',self[company])
-        print('printing',self.apple)
 
-        # if company == 'Apple':
-        #     if self.apple.is_empty():
-        #         print('You do not have Apple stock.')
-        #         return
-        #     pop = self.apple.pop()
-        #     if pop[0] >= number:
-        #         shares_left = pop[0] - number
-        #         profit = ((number*sell_price) - (number*pop[1]))
-        #         self.total_profit += profit
-        #         if shares_left != 0:
-        #             self.apple.push([shares_left, pop[1]])
-        #         return 
-        #     elif pop[0] < number:
-        #         number -= pop[0]
-        #         profit = ((pop[0]*sell_price) - (pop[0]*pop[1]))
-        #         self.total_profit += profit
-        #         return self.sell_share('Apple', number, sell_price)
-            
-        # elif company == 'Google':
-        #     self.google.push([number, buy_price])
-        #     pass
-        # elif company == 'BMW':
-        #     self.bmw.push([number, buy_price])
+        if self[company].is_empty():
+            print(f'You do not have stock of {company}.')
+            print(profit, 'lol')
+            return profit
+
+        pop = self[company].pop()
+
+        if pop[0] < number:
+            print('p0 less','stack: ',pop[0], ' n:', number, ' profit: ', profit)
+            number -= pop[0]
+            profit += (pop[0]*sell_price) - (pop[0]*pop[1])
+            return self.sell_share(company, number, sell_price, profit)            
+        elif pop[0] >= number:
+            print('p0 more', 'stack: ',pop[0], ' n:', number, ' profit: ', profit)
+            shares_left = pop[0] - number
+            profit += (number*sell_price) - (number*pop[1])
+            self[company].push([shares_left, pop[1]])
+            return profit
         
-   
 
     # def sell_multiple(self, company_list, number_list, sell_price_list):
 
 manager = StockManager()
 manager.buy_shares("Apple", 10, 5)
-manager.buy_shares("Google", 120, 5)
-manager.buy_shares("BMW", 110, 5)
+manager.buy_shares("Apple", 10, 5)
+manager.buy_shares("Apple", 10, 5)
+
+print(manager.sell_share('Apple', 25, 7))
 manager.apple.print_contents()
-manager.google.print_contents()
-manager.bmw.print_contents()
-# manager.sell_share('Apple', 100, 10)
-# manager.apple.print_contents()
-print(manager.get_profit())
